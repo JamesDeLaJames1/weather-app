c7f3d7bd5f012fbf42ac972bf6b0e94d26deddf3
James Kemp <j.h.kemp462@gmail.com>
2022-04-05 22:18:54 +0100

    register change

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,6 +20,21 @@ function Register() {
 
     });
 
+    function getCookie(name) {
+        if (!document.cookie) {
+          return null;
+        }
+      
+        const xsrfCookies = document.cookie.split(';')
+          .map(c => c.trim())
+          .filter(c => c.startsWith(name + '='));
+      
+        if (xsrfCookies.length === 0) {
+          return null;
+        }
+        return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+      }
+
     async function register(){
         //Register user from input data handle errors
         let creds = {name, email, location, password};
